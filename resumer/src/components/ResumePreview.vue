31a96952f2822b1a015baa7b1e3c85423b7d1c24
Hsyneve <906592157@qq.com>
2017-02-08 17:03:30 +0800

    resumer

---
+++ resumer/src/components/ResumePreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="resumePreview">
+     I am resumePreview
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'resumePreview',
+  data: function(){
+    return {
+  text: '你好'
+  }
+  }
+
+}
+</script>
+
+<style>
+
+#resumePreview
+{box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);}
+</style>
---
+++ resumer/src/components/resumeEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="resumeEditor">
+     I am resumeEditor
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'resumeEditor',
+  data: function(){
+    return {
+  text: '你好'
+  }
+  }
+
+}
+</script>
+
+<style>
+
+#resumeEditor
+{
+box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);}
+</style>
